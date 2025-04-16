fe6a623ebc67f555e40337e6313481167764b4d8
code2hts <collinslm.vijey@gmail.com>
2025-04-16 19:14:22 +0530

    Update pipeline.py

--- Radio_Fingerprint_project/radio_fingerprint_model/pipeline.py
+++ Radio_Fingerprint_project/radio_fingerprint_model/pipeline.py
@@ -16,39 +16,39 @@ from bikeshare_model.processing.features import OutlierHandler, WeekdayOneHotEnc
 bikeshare_pipe = Pipeline([
 
     ######### Imputation ###########
-    ('weekday_imputation', WeekdayImputer(variable = config.model_config_.weekday_var, 
+    #('weekday_imputation', WeekdayImputer(variable = config.model_config_.weekday_var, 
                                           date_var= config.model_config_.date_var)),
-    ('weathersit_imputation', WeathersitImputer(variable = config.model_config_.weathersit_var)),
+    #('weathersit_imputation', WeathersitImputer(variable = config.model_config_.weathersit_var)),
     
     ######### Mapper ###########
-    ('map_yr', Mapper(variable = config.model_config_.yr_var, mappings = config.model_config_.yr_mappings)),
+    #('map_yr', Mapper(variable = config.model_config_.yr_var, mappings = config.model_config_.yr_mappings)),
     
-    ('map_mnth', Mapper(variable = config.model_config_.mnth_var, mappings = config.model_config_.mnth_mappings)),
+    #('map_mnth', Mapper(variable = config.model_config_.mnth_var, mappings = config.model_config_.mnth_mappings)),
     
-    ('map_season', Mapper(variable = config.model_config_.season_var, mappings = config.model_config_.season_mappings)),
+    #('map_season', Mapper(variable = config.model_config_.season_var, mappings = config.model_config_.season_mappings)),
     
-    ('map_weathersit', Mapper(variable = config.model_config_.weathersit_var, mappings = config.model_config_.weathersit_mappings)),
+    #('map_weathersit', Mapper(variable = config.model_config_.weathersit_var, mappings = config.model_config_.weathersit_mappings)),
     
-    ('map_holiday', Mapper(variable = config.model_config_.holiday_var, mappings = config.model_config_.holiday_mappings)),
+    #('map_holiday', Mapper(variable = config.model_config_.holiday_var, mappings = config.model_config_.holiday_mappings)),
     
-    ('map_workingday', Mapper(variable = config.model_config_.workingday_var, mappings = config.model_config_.workingday_mappings)),
+    #('map_workingday', Mapper(variable = config.model_config_.workingday_var, mappings = config.model_config_.workingday_mappings)),
     
-    ('map_hr', Mapper(variable = config.model_config_.hr_var, mappings = config.model_config_.hr_mappings)),
+    #('map_hr', Mapper(variable = config.model_config_.hr_var, mappings = config.model_config_.hr_mappings)),
     
     ######## Handle outliers ########
-    ('handle_outliers_temp', OutlierHandler(variable = config.model_config_.temp_var)),
-    ('handle_outliers_atemp', OutlierHandler(variable = config.model_config_.atemp_var)),
-    ('handle_outliers_hum', OutlierHandler(variable = config.model_config_.hum_var)),
-    ('handle_outliers_windspeed', OutlierHandler(variable = config.model_config_.windspeed_var)),
+    #('handle_outliers_temp', OutlierHandler(variable = config.model_config_.temp_var)),
+    #('handle_outliers_atemp', OutlierHandler(variable = config.model_config_.atemp_var)),
+    #('handle_outliers_hum', OutlierHandler(variable = config.model_config_.hum_var)),
+    #('handle_outliers_windspeed', OutlierHandler(variable = config.model_config_.windspeed_var)),
 
     ######## One-hot encoding ########
-    ('encode_weekday', WeekdayOneHotEncoder(variable = config.model_config_.weekday_var)),
+    #('encode_weekday', WeekdayOneHotEncoder(variable = config.model_config_.weekday_var)),
 
     # Scale features
-    ('scaler', StandardScaler()),
+    #('scaler', StandardScaler()),
     
     # Regressor
-    ('model_rf', RandomForestRegressor(n_estimators = config.model_config_.n_estimators, 
+    ('model_fingerprint', RandomForestRegressor(n_estimators = config.model_config_.n_estimators, 
                                        max_depth = config.model_config_.max_depth,
                                       random_state = config.model_config_.random_state))
     
