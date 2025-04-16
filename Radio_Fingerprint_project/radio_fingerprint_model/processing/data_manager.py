2b1c90a6a0e5336054e5cc1483bef18df8c6b409
code2hts <collinslm.vijey@gmail.com>
2025-04-16 14:40:39 +0000

    Resolved bikeshare copy paste errors

--- Radio_Fingerprint_project/radio_fingerprint_model/processing/data_manager.py
+++ Radio_Fingerprint_project/radio_fingerprint_model/processing/data_manager.py
@@ -51,8 +51,8 @@ def _load_raw_dataset(*, file_name: str) -> pd.DataFrame:
     return dataframe
 
 def load_dataset(*, file_name: str) -> pd.DataFrame:
-    dataframe = pd.read_csv(Path(f"{DATASET_DIR}/{file_name}"))
-    transformed = pre_pipeline_preparation(data_frame = dataframe)
+    transformed = dataframe = pd.read_csv(Path(f"{DATASET_DIR}/{file_name}"))
+    #transformed = pre_pipeline_preparation(data_frame = dataframe)
 
     return transformed
 
